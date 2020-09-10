c119ba68c1921dd4d9a4d1f6f2641edb257fe35b
yassinerocmaaa <yassine.el-asri@polymtl.ca>
2020-09-10 22:10:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    distance_entre_min_et_maj= prd
+    distance_entre_min_et_maj= prd('a')
     resultat = ''
     for lettre in mot:
         # 
